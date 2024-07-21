8f85216cc9f55b8ac44e0346b739f0f936ba32ed
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2024-07-21 19:31:33 +0530

    Updated frontend

---
+++ src/app/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "@/app/styles"
+
+
+const header = () => {
+  return (
+<header className={styles.main_header}>
+    <div className={styles.navbar_brand}></div>
+</header>
+  )
+}
+
+export default header
