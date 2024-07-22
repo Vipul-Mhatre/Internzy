704bfa29ce36a885cb381000636d0f9d4f583e1f
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2024-07-22 08:28:26 +0530

    soved frontend issues

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import styles from "@/app/styles"
+import React from 'react';
+import styles from "@/app/styles/navbar.module.css";
+import Link from 'next/link';
+import Image from 'next/image'; // Import the Image component
 
-
-const header = () => {
+const Header = () => {
   return (
-<header className={styles.main_header}>
-    <div className={styles.navbar_brand}></div>
-</header>
-  )
-}
+    <header className={styles.main_header}>
+      <div className={styles.navbar_brand}>
+        <Link href="/">
+          <Image src="/Suvidhalogo" alt="logo" width={150} height={50} /> {/* Use Image component */}
+        </Link>
+      </div>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
 
-export default header
+export default Header;
